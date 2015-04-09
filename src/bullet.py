e3fe4013a21d5e7c4382f455a51985b1bbcfc3fb
Ethan R <ethanx94@gmail.com>
2015-04-09 01:13:40 -0500

    bullets

--- src/bullet.py
+++ src/bullet.py
@@ -6,7 +6,7 @@ from state import state
 class bullet(pygame.Rect):
     __bullets = []
     
-    def __init__(self, xpos, ypos, wid, ht, xspeed, yspeed, screen, state):
+    def __init__(self, xpos, ypos, wid, ht, xspeed, yspeed, screen, state, playerDirection):
         super(bullet, self).__init__(xpos, ypos-14, wid, ht)
         self.__xpos = xpos
         self.__ypos = ypos
@@ -20,8 +20,20 @@ class bullet(pygame.Rect):
         self.__r = 255
         self.__g = 255
         self.__b = 255
-        
         self.__fire = False
+        self.drawIt()
+       
+        #self.y -= self.__yspeed
+        '''
+        if playerDirection == 'MOVE_DOWN':
+            self.y += self.__yspeed
+        elif playerDirection == 'MOVE_UP':
+            self.y -= self.__yspeed
+        elif playerDirection == 'MOVE_LEFT':
+            self.x += self.__xspeed
+        elif playerDirection == 'MOVE_RIGHT':
+            self.x -= self.__xspeed
+        '''
         
     def checkIt(self, player, boundLeft, boundRight, boundTop):
         for bullet in self.__bullets:        
@@ -29,9 +41,12 @@ class bullet(pygame.Rect):
                 self.__bullets.remove(bullet)
         
     def drawIt(self):
+        pygame.draw.rect(self.__screen, (self.__r, self.__g, self.__b), self)
+        '''
         for bullet in self.__bullets:
             pygame.draw.rect(self.__screen, (self.__r, self.__g, self.__b), bullet)
-    
+        '''
+        
     def moveIt(self, playerDirection):
         if self.__fire == True:
             if playerDirection == 'MOVE_DOWN':
@@ -48,5 +63,5 @@ class bullet(pygame.Rect):
         self.__bullets.append(self)
         self.drawIt()
         print self.__bullets
-        
+    
             
